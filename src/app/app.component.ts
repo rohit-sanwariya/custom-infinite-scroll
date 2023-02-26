b621f0a1826f0ed5236ce767b41aa88cf13df42d
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-02-26 16:13:06 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'declarative-on-push';
+  pagination:{pagenumber:number,limit:number}={
+    pagenumber:1,limit:10,
+  }
+  photos$ : Observable<Array<any>> = this._http.get<Array<any>>(`http://localhost:3000/photos?_page=${this.pagination.pagenumber}&_limit=${this.pagination.limit}`)
+  constructor(
+    private _http:HttpClient
+  ){}
+
+  onScroll(){
+    console.log('end')
+  }
 }
